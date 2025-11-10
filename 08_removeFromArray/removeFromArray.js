d5b1bea25797b35388af48c18ed982571efdb500
Nandan <230424156+nandxi@users.noreply.github.com>
2025-11-10 13:58:02 +0530

    Solve removeFromArray.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    for (let item of items) {
+        while (array.includes(item)) {
+            array.splice(array.indexOf(item), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
