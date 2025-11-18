7934c812244050e738c656f78ddfcd71f32b6b97
Nandan <230424156+nandxi@users.noreply.github.com>
2025-11-18 18:07:47 +0000

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+  const punctuations = [".", ",", "!", " "];
+  const sentenceArr = sentence.split("").filter((character) => !punctuations.includes(character));
+  const newSentence = sentenceArr.join("");
+  const reversedSentence = sentenceArr.reverse().join("");
+  return newSentence.toLowerCase() === reversedSentence.toLowerCase();
 
 };
 
