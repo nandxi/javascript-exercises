1e07db421237f1507a8177d38245d0289c198150
Nandan <230424156+nandxi@users.noreply.github.com>
2025-11-18 17:32:59 +0000

    Solve 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums.length === 0) return 0;
+	return nums.reduce((total, num) => total + num);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+	while (num >= 2) {
+    fact *= num;
+    num--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
