d313c5ed30aa494b5203fb839c521cc2f6456c69
Nandan <230424156+nandxi@users.noreply.github.com>
2025-11-19 07:29:32 +0000

    Solve 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = Number(n);
+  if (n < 0) return "OOPS";
+  else if (n === 0) return 0;
+  else if (n === 1 || n === 2) return 1;
 
+  let a = 1;
+  let b = 1;
+  let currNum = 0;
+  for (let i = 2; i < n; i++) {
+    currNum = a + b;
+    b = a;
+    a = currNum;
+  }
+  return currNum;
 };
 
 // Do not edit below this line
