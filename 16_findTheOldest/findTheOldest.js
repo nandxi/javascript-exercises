4fd445c977852d7c9c2131acb20f5f15b06916ec
Nandan <230424156+nandxi@users.noreply.github.com>
2025-11-19 08:12:54 +0000

    Solve findTheOldest.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  const oldest = arr.reduce((lastPerson, nextPerson) => {
+    if (!lastPerson.yearOfDeath) lastPerson.yearOfDeath = (new Date()).getFullYear();
+    if (!nextPerson.yearOfDeath) nextPerson.yearOfDeath = (new Date()).getFullYear();
+    let lastPersonAge = lastPerson.yearOfDeath - lastPerson.yearOfBirth;
+    let nextPersonAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+    return ((lastPersonAge > nextPersonAge) ? lastPerson : nextPerson);
+  });
+  return oldest;
 };
 
 // Do not edit below this line
