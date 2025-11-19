42ed252031c4518d8633f02552c7c4390d7c614c
Nandan <230424156+nandxi@users.noreply.github.com>
2025-11-19 07:47:52 +0000

    Understand the solution given in solution folder

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -2,17 +2,16 @@ const fibonacci = function(n) {
   n = Number(n);
   if (n < 0) return "OOPS";
   else if (n === 0) return 0;
-  else if (n === 1 || n === 2) return 1;
 
   let a = 1;
-  let b = 1;
-  let currNum = 0;
-  for (let i = 2; i < n; i++) {
+  let b = 0;
+  let currNum;
+  for (let i = 2; i <= n; i++) {
     currNum = a + b;
     b = a;
     a = currNum;
   }
-  return currNum;
+  return a;
 };
 
 // Do not edit below this line
